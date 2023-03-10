dfb68c7a3a5702b0f4919d8b2c692378e43c9283
westonh2 <weston@full-circle.design>
2023-03-09 22:51:34 -0500

    hero

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php $pageTitle = 'Design Lab Home'; ?>
 <?php include('component-head.php'); ?>
 
+<img class="hero" src="assets/sage.jpg">
+<div class="hero-section">
+  <div class="hero-section__text-group">
+    <div class="hero-section__text-group__item hero-section__text-group__item_1">WELCOME&nbsp</div>
+    <div class="hero-section__text-group__item hero-section__text-group__item_2">TO</div>
+  </div>
+  <br>
+  <div class="hero-section__text-group__item hero-section__text-group__item_3">westonhuggins.com</div>
+</div>
+
+
 <ul class="index-list">
   <li class="index-li"><a class="index-link" href="games.php"><span>Games</span></a></li>
   <li class="index-li"><a class="index-link" href="scroll-pages.html"><span>Scroll Pages</span></a></li>
